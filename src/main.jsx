e8d11d38102a247fe63de7990b91d56c9f7d3988
Subhajit Mandal <smandal0511@gmail.com>
2022-11-18 20:15:37 +0530

    Remove jotai dep

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider as JotaiProvider } from "jotai";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <JotaiProvider>
-    <App />
-  </JotaiProvider>
+  <App />
   // </React.StrictMode>
 );
